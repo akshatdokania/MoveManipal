843d2a5afcc4de4fecdc2aebf8790fb7163f1507
akshat <akshatdokania7@gmail.com>
2021-06-03 23:01:57 +0530

    Initial Object

---
+++ move_manipal/lib/object.dart
@@ -0,0 +1,112 @@
+class Head{
+  var places = <String,Map<String,List>>{
+    'Inside Manipal' : {
+      'Sri Krishna Temple': [],
+      'Delta Beach': [],
+      'Manipal Museum of Anatomy': [],
+      'Hoode Beach': [],
+      'End Point': [],
+      'Malpe Beach': [],
+      'Hasta Shilpa Heritage Village': [],
+      'Manipal Lake': [],
+      'Arbi Falls': [],
+      'Venugopal Temple': [],
+      'Hanging Bridge': [],
+      'Kaup Beach': [],
+      'St. Mary Island': [],
+    },
+    'Vicinity': {
+      'Agumbe': [],
+      'Gokarna': [],
+      'Goa': [],
+      'Mangalore': [],
+      'Kundadri': [],
+      'Jog Falls': [],
+    }
+  };
+  var food = <String,Map<String,List>>{
+    'Family Restraunts':{
+      'Saiba' : [],
+      'Dollops' : [],
+      'Egg Factory': [],
+      'Eye of the Tiger': [],
+      'Snack Shack': [],
+      'Sizzler Ranch':[],
+      'Pai Tiffins': [],
+      'Basil Cafe':[],
+      'Cafe7Bees':[],
+      'Planet Cafe': [],
+      'Laughing Buddha':[],
+      'MIT Cafeteria':[],
+      'FC2':[],
+      'FC1':[],
+    },
+    'Indulgence': {
+      'KFC':[],
+      'Dominoz':[],
+      'McDonalds':[],
+      'Froth On Top':[],
+      'Hastag':[],
+      'Subway':[],
+      'BarbeQ Nation':[],
+      'Tiwari Chaat':[],
+      'MFC':[],
+      'Grub Monkeys':[],
+      'Rolls Mania':[],
+      'DeeTee':[],
+      'Zeal':[],
+      'Edge':[],
+      'Black Cup':[],
+    },
+    'Desserts':{
+      'Belgian Waffle':[],
+      'CadM CadB':[],
+      'Naturals':[],
+      'Hangyos':[],
+    }
+  };
+  var essentials = <String,Map<String,List>>{
+    'Grocery Stores':{
+      'Student Plaza':[],
+      'Campus Store':[],
+      'Yas Mart':[],
+      'BigBazaar (Udupi)':[],
+    },
+    'Clothing Stores':{
+      'Reliance Trendz':[],
+      'Adidas':[],
+      'Puma':[],
+      'Trends(Canara Mall)':[],
+      'Max':[],
+    },
+    'Medical Stores':{
+      'KMC':[],
+      'Radha Medicals':[],
+      'Manipal Drug House':[],
+      'Apollo Pharmacy':[],
+    },
+    'Hotels&Inns':{
+      'Country Inn':[],
+      'Green Park Hotel':[],
+      'Hotel Tranquil':[],
+      'Hotel Madhuvan Sarai':[],
+      'HomeTown Galleria':[],
+      'Fortune Inn Valley View':[],
+      'Hotel Central Park':[],
+      'The Ocean Pearl':[],
+      'Oyo':[],
+    },
+  };
+  var vehicle = <String,Map<String,List>>{
+    'Rentals': {
+      'Zoomcar':[],
+      'BhoomRiderz':[],
+      'Scootys':[],
+    },
+    'Transport':{
+      'Bus':[],
+      'Auto':[],
+      'Cab' : [],
+    },
+  };
+}
